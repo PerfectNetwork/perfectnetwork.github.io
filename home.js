30d1b5e3b92a0f02b7f0f20861c17096f4c88d27
Perfect-Network <160994786+PerfectNetwork@users.noreply.github.com>
2024-11-22 14:32:34 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+// تحميل النصوص المتحركة
+document.addEventListener('DOMContentLoaded', function () {
+    new Typed('#typedtext', {
+        strings: [
+            "حيث يتجسد الإبداع والتميز",
+            "رحلة لا تُنسى من الاستمتاع والسعادة",
+            "بوابتك لعالم افتراضي مليء بالمتعة والاثاره"
+        ],
+        typeSpeed: 50,
+        backSpeed: 25,
+        loop: true
+    });
+});
