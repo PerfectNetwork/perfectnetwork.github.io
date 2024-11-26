0601ebb18664141dc8a7796974911b0dc3cc7c5e
Perfect-Network <160994786+PerfectNetwork@users.noreply.github.com>
2024-11-26 20:17:46 +0200

    Update home.js

--- home.js
+++ home.js
@@ -11,3 +11,29 @@ document.addEventListener('DOMContentLoaded', function () {
         loop: true
     });
 });
+
+// العد التصاعدي للأرقام مع إضافة "+" لبعض الأرقام
+document.addEventListener("DOMContentLoaded", function() {
+    const counters = document.querySelectorAll('.stat-number');
+    
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-count');
+            let count = +counter.innerText;
+            const increment = target / 200;
+            
+            if (count < target) {
+                counter.innerText = Math.ceil(count + increment); // زيادة العد تدريجيا
+                setTimeout(updateCount, 10); // التحديث بعد 10 ميلي ثانية
+            } else {
+                // إضافة "+" فقط إذا كانت القيمة هي 120 أو 130
+                if (target === 120 || target === 130) {
+                    counter.innerText = target + ' +';
+                } else {
+                    counter.innerText = target;
+                }
+            }
+        };
+        updateCount();
+    });
+});
